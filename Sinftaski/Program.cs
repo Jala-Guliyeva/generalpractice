9f135ff0e2afbd021807e7f3e7b72d48769ec53a
Jala Guliyeva <tu7knjupn@aztu.local>
2022-04-01 01:19:12 -0700

    file added

---
+++ Sinftaski/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Sinftaski
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue <string> q = new Queue <string>();
+            q.Enqueue("Jale");
+            q.Enqueue("Kamal");
+            q.Enqueue("Nergiz");
+
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
+            Stack(q);
+        
+          
+            
+        }
+        public Queue<string> Stack(Queue<string> queue)
+        {
+
+            Stack<string> stack = new Stack<string>();
+            foreach (var item in queue)
+            {
+                stack.Push(item);
+            }
+            
+            Queue<string> queue2 = new Queue<string>();
+            foreach (var item in stack)
+            {
+                queue2.Enqueue(item);
+            }
+            return queue2;
+            
+        }
+    }   
+    
+}
